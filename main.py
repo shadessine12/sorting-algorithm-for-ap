08ab3f66271a13729dd8b821a76c87f1bc1ca857
shadessine12 <108633464+shadessine12@users.noreply.github.com>
2023-03-08 14:28:02 -0800

    Added Bubble Sort function

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def bubbleSort(list):
     while order(list) == false:
+        for i in range(0, list.length):
+            if list[i] > list[i + 1]:
+                list.insert(i, i + 1)
+                list.pop(i + 2)
+    return list
 
 
 def order(list):
