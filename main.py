220bbecaa62db7e18299a27541d0ce1196daf576
shadessine12 <108633464+shadessine12@users.noreply.github.com>
2023-03-08 15:15:38 -0800

    Added Selection Sort

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+#Credit to Geeks for Geeks Website for pseudocode on how to do selection sort
+def selectionSort(list):
+    for i in range(0, len(list)):
+        minIndex = i
+        for j in range(i + 1, len(list)):
+            if list[j] < list[minIndex]:
+                minIndex = j
+        list[minIndex], list[i] = list[i], list[minIndex]
+    return list
+
+
 def bubbleSort(list):
     while order(list) == false:
-        for i in range(0, list.length):
+        for i in range(0, len(list)):
             if list[i] > list[i + 1]:
                 list.insert(i, i + 1)
                 list.pop(i + 2)
@@ -9,10 +20,10 @@ def bubbleSort(list):
 
 def order(list):
     truthCounter = 0
-    for i in range(0, list.length):
+    for i in range(0, len(list)):
         if list[i] > list[i - 1] && list[i] < list[i + 1]:
             truthCounter = truthCounter + 1
-    if truthCounter == list.length:
+    if truthCounter == len(list):
         return true 
     else:
         return false
